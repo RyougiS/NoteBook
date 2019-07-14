4c70b6fd8f34dbd122dc750f32f7fd7d3b95b407
RyougiS <deathzmh@hotmail.com>
2019-07-14 15:17:41 +0800

    update .gitignore

--- TypeScript/1.ts
+++ TypeScript/1.ts
@@ -1,2 +1,13 @@
 var str:string = "abc"
 console.log(str)
+
+//定义数组
+let arr:number[] = [1,2,3,4]
+let arr2:object[] = [{
+    a:1
+},{
+    b:2
+},{
+    c:3
+}]
+
--- TypeScript/src/1.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var str = "abc";
-console.log(str);
